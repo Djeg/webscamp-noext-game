ad4ccb17cdca27b513d4118ae4cfdc8e54f9b96a
David Jegat <david.jegat@knplabs.com>
2016-09-01 15:56:38 +0200

    Remove entity repository

--- src/App/Symfony/Repository/RecipesList.php
+++ src/App/Symfony/Repository/RecipesList.php
@@ -4,23 +4,24 @@ namespace App\Symfony\Repository;
 
 use Doctrine\ORM\EntityRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\QueryBuilder;
 
-class RecipesList extends EntityRepository
+class RecipesList
 {
+    private $queryBuilder;
+
+    public function __construct(QueryBuilder $queryBuilder)
+    {
+        $this->queryBuilder = $queryBuilder;
+    }
+
     /**
-     * @param Request $request
-     *
      * @return \App\Symfony\Entity\Recipe[]
      */
-    public function findAllAndPaginate(Request $request)
+    public function findAll()
     {
-        $offset = (int)$request->query->get('offset', 0);
-        $limit = (int)$request->query->get('limit', 25);
-        $orderBy = $request->query->get('orderBy', 'r.id');
-
         return $this
-            ->createQueryBuilder('r')
-            ->orderBy($orderBy)
+            ->queryBuilder
             ->getQuery()
             ->getResult()
         ;
